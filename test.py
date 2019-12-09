9c272fbb0bb29abb4b1f4baaaa84163ee7ec82ed
Zumcern <magnus_sumstad@hotmail.com>
2019-12-09 09:31:15 +0000

    test1

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello World)
+
+user_number  = input("Enter your number ")
+if user_number.isdigit():
+    user_number = int(user_number) * 2
+    print("int times two is " + str(user_number))
+else:
+    print("User input is string ")
+            
